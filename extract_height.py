306b8cf35471e5be412489800adda4bc22778181
Andre Theron <33958609+andretheronsa@users.noreply.github.com>
2019-08-13 15:11:23 +0200

    Update extract_height.py

--- extract_height.py
+++ extract_height.py
@@ -1,12 +1,7 @@
-############################################################
-# Project: Extract height from bulk downloaded zip files   #
-# Author: Andre Theron (andretheronsa@gmail.com)           #
-# Created: July 2019                                       #
-############################################################
-
 import os
 import zipfile
 import csv
+import datetime
 
 # Find input files in cwd
 inputfiles = []
@@ -23,16 +18,18 @@ for inputfile in inputfiles:
                 if archive_file.endswith("q"):
                     q_file = archive_file
                     break
-            date = "".join(["20", q_file.split('.')[1].split('q')[0], q_file.split('N')[1].split('Z')[0]])
+            julian_date = q_file.split('N')[1].split('Z')[0]
+            year = "".join(["20", q_file.split('.')[1].split('q')[0]])
+            date = datetime.datetime.strptime(year + julian_date, '%Y%j')
             with archive.open(q_file) as data:
                 for line in data:
                     if "WGS 84 height" in str(line):
                         height = str(line).split(":")[1].split(" ")[1]
-                        heights[date] = height
+                        heights[str(date.date())] = height
     except Exception as e:
         print("{} {}".format(inputfile, e))
 
 # Write to file
 with open('GPS_heights.txt', 'w', newline='') as f:
     w = csv.writer(f)
-    w.writerows(heights.items())
+    w.writerows(sorted(heights.items()))
