8fffd0b27b2a8c8a39dee70416b99206e0cf6fbb
Andre Theron <33958609+andretheronsa@users.noreply.github.com>
2019-08-14 15:54:24 +0200

    Update extract_height.py
    play

--- extract_height.py
+++ extract_height.py
@@ -2,6 +2,10 @@ import os
 import zipfile
 import csv
 import datetime
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+import numpy as np
 
 # Find input files in cwd
 inputfiles = []
@@ -10,7 +14,7 @@ for inputfile in os.listdir():
         inputfiles.append(inputfile)
 
 # Extract heights
-heights = {}
+ts = {}
 for inputfile in inputfiles:
     try:
         with zipfile.ZipFile(inputfile, 'r') as archive:
@@ -25,9 +29,25 @@ for inputfile in inputfiles:
                 for line in data:
                     if "WGS 84 height" in str(line):
                         height = str(line).split(":")[1].split(" ")[1]
-                        heights[str(date.date())] = height
+                        ts[date.date()] = {"height": float(height)}
     except Exception as e:
         print("{} {}".format(inputfile, e))
+
+# Pandas work
+df = pd.DataFrame.from_dict(ts, orient="index")
+
+# Remove outliers
+# Quantiles
+q1 = df.quantile(0.25)
+q3 = df.quantile(0.75)
+iqr = q3 - q1
+
+# z-score
+z = np.abs(stats.zscore(df))
+df_z = df[(z < 2).all(axis=1)]
+
+# Average
+df_z_a = df_z.rolling(3).median()
 
 # Write to file
 with open('GPS_heights.txt', 'w', newline='') as f:
